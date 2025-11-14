3362d690c286930d27bb06c694684346d18ad6b0
Mirren Shah <shahmirren@gmail.com>
2025-11-14 16:49:47 +0000

    ran composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mizzalini/task-manager',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd55725259c9aa8537729309e7c286edbeca4eba1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mizzalini/task-manager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd55725259c9aa8537729309e7c286edbeca4eba1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
