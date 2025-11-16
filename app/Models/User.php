c46a48b10bb234dea9a90799201da4dab8ccfde2
Mirren Shah <shahmirren@gmail.com>
2025-11-16 22:53:37 +0000

    Added User model

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TaskManager\Models;
+
+/**
+ * User model
+ */
+class User {
+
+    public string $user = "admin";
+    public string $pass = '$2y$10$IdBRJ1Qfdm7kcwotBdpo6.eSiv4gDbpWoYDSYrs9VQjU13YRFKWPO';
+}
