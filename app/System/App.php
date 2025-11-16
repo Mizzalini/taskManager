d85ee65bf8e408ad9d08ce57f5b66317479da4a2
Mirren Shah <shahmirren@gmail.com>
2025-11-16 02:49:52 +0000

    added System/App.php

---
+++ app/System/App.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TaskManager\System;
+use TaskManager\Traits\Singleton;
+
+/**
+ * Handles user navigation requests, executing the appropriate controller
+ */
+class App {
+    use Singleton;
+
+    public function run(): void {
+        
+    }
+}
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-?>
+$app = \TaskManager\System\App::instance();
+echo $app->run();
