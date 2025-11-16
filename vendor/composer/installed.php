5ada2873370a0292d43d4e28e45a47dfbf8f96b3
Mirren Shah <shahmirren@gmail.com>
2025-11-16 01:16:22 +0000

    added composer.json and applied autoload settings

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mizzalini/task-manager',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '57fe72e91de242717e2d98a70e9430336e4927be',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mizzalini/task-manager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '57fe72e91de242717e2d98a70e9430336e4927be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
