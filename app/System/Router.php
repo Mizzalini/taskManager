467a3da9d875005493e3bb040023dc6824f25888
Mirren Shah <shahmirren@gmail.com>
2025-11-17 02:39:02 +0000

    Added run() and getCurrentPath() methods in Router.php to handle routes

--- app/System/Router.php
+++ app/System/Router.php
@@ -15,6 +15,33 @@ class Router {
 
     private array $routes = [];
 
+    /**
+     * Runs the method associated with the current path
+     * 
+     * @return string|false The output of the controller method or false on failure
+     */
+    public function run(): string|false {
+        $path = $this->getCurrentPath();
+        $controllerMapping = $this->routes[$path] ?? null;
+
+        if ($controllerMapping) {
+            [$controllerClass, $method] = $controllerMapping;
+            $controller = new $controllerClass();
+            return $controller->$method();
+        }
+
+        return View::render('layouts/page404.php');
+    }
+
+    /**
+     * Gets the current request path
+     * 
+     * @return string The current request path
+     */
+    public function getCurrentPath(): string {
+        return \parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
+    }
+
     private function init(): void {
         $this->routes = require \getcwd() . '/app/routes.php';
     }
