300c525e26192717d9c105956b1cc4bd00c00a9a
Mirren Shah <shahmirren@gmail.com>
2025-11-17 15:26:04 +0000

    Simplified implementation by removing changed index() method

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,18 +1,7 @@
 <?php
 
 namespace TaskManager\Controllers;
+
 use TaskManager\System\Controller;
 
-class Home extends Controller {
-
-    /**
-     * Renders the home page with a list of tasks
-     * 
-     * @param array $data Additional data to pass to the view
-     * @return string|false The rendered home page content or 'false' on failure
-     */
-    public function index(array $data = []): string|false {
-        $tasks = $_SESSION['tasks'] ?? [];
-        return parent::index(['tasks' => $tasks]);
-    }
-}
+class Home extends Controller {}
